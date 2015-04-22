411bcb10501f95e9774d8a0609f49ca6a6565d05
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2015-04-22 20:18:58 +0200

    Construct looking classes

--- ownlib/look/eye.py
+++ ownlib/look/eye.py
@@ -2,3 +2,6 @@
 __author__ = 'ArchieT'
 class eye:
 	pass
+class shot:
+	def __repr__(self): return self.w
+	def __str__(self): return self.__repr__()
--- ownlib/look/eyes/keyboardtyping.py
+++ ownlib/look/eyes/keyboardtyping.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ArchieT'
-from ownlib.look.eye import eye
-class keyboardtyping(eye): pass
+from ownlib.look.eye import eye,shot
+class keyboardtyping(eye):
+	def __init__(self): pass
+	def __enter__(self):
+		print "Hej"
+		return self
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		print "keyboardtyping.__exit__",exc_type,exc_val,exc_tb
+	def giveshot(self): return keybtext(self)
+class keybtext(shot):
+	def __init__(self,eye): pass
+	def __enter__(self):
+		self.w = raw_input("Hej, jestem jednym stanem, daj FENa:  ")
+		return self
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		print "keybtext.__exit__",exc_type,exc_val,exc_tb
