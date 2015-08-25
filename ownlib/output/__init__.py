cf6b1e2c35f43293433b5ed70586fa7b12abbf5d
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-08-25 13:27:40 +0200

    Output module structure

--- ownlib/output/__init__.py
+++ ownlib/output/__init__.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ArchieT'
+class Output:
+	pass
+class OutputMove:
+	def __init__(self,maszyna,typ,skad,dokad):
+		assert 'PROPERMACHINE' in dir(self)
+		assert isinstance(maszyna,self.PROPERMACHINE),"not isinstance(maszyna,%s)" % str(self.PROPERMACHINE)
+		self.maszyna = maszyna
+		self.typ = typ
+		self.skad = skad
+		self.dokad = dokad
+class OutputFigure:
+	def __init__(self,maszyna,figheight,pullheight):
+		assert 'PROPERMACHINE' in dir(self)
+		assert isinstance(maszyna,self.PROPERMACHINE),"not isinstance(maszyna,%s)" % str(self.PROPERMACHINE)
+		self.figheight = figheight
+		self.pullheight = pullheight
