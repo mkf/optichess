16f3a3691ac479e512b0df6aba1aade357af7225
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-09-11 09:23:41 +0200

    Working on moving in output.reality.linear

--- ownlib/output/reality/linear.py
+++ ownlib/output/reality/linear.py
@@ -11,13 +11,17 @@ class Linear(Reality):
             homepos=DEFHOMEPOS,
             pos=DEFHOMEPOS,
             filedegperone=100,
-            rankdegperone=100):
+            rankdegperone=100,
+            bspeed=127):
+        self.bspeed=bspeed
         self.filedegperone=filedegperone
         self.rankdegperone=rankdegperone
         Reality.__init__(self,kostkaid=kostkaid,homepos=homepos,pos=pos)
    def goto(self,loc):
         print "goto ",loc
         movin=(loc[0]-self.pos[0],loc[1]-self.pos[1])
-        self.motfile.turn(self.filedegperone*movin[0])
-        self.motrank.turn(self.rankdegperone*movin[1])
+        filesp=self.bspeed*(1 if movin[0]>0 else 0 if movin[0]==0 else -1 if movin[0]<0 else None)
+        ranksp=self.bspeed*(1 if movin[1]>0 else 0 if movin[1]==0 else -1 if movin[1]<0 else None)
+        if filesp!=0: self.motfile.turn(filesp,abs(self.filedegperone*movin[0]))
+        if ranksp!=0: self.motrank.turn(ranksp,abs(self.rankdegperone*movin[1]))
         self.pos=loc
